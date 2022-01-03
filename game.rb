736709c189f5b8bea3b4bd18ef66bc988c0b8b7e
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2022-01-03 23:08:46 +0000

    Add end_of_turn and end_of_game variables

--- game.rb
+++ game.rb
@@ -1,10 +1,12 @@
 class Game
-  attr_reader :current_player, :right_answer, :wrong_answer
+  attr_reader :current_player, :right_answer, :wrong_answer, :end_of_turn, :end_of_game
 
   def initialize()
     @current_player = "Player 1"
     @right_answer = "You are correct!"
     @wrong_answer = "Your answer is incorrect!"
+    @end_of_turn = "----- NEW TURN -----"
+    @end_of_game = "----- GAME OVER ----- \nGood bye!"
   end
 
   def next_player
